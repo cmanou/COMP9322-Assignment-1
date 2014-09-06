2ecabb76493064c7365bcb2b3cf1158a2922e1cb
Christopher Manouvrier <cmanou@mac.com>
2014-09-06 14:08:49 +1000

    Initial Axis template

---
+++ CurrencyConvertService/src/au/edu/unsw/sltf/services/CurrencyConvertMarketDataServiceSkeleton.java
@@ -0,0 +1,33 @@
+
+/**
+ * CurrencyConvertMarketDataServiceSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.2  Built on : Apr 17, 2012 (05:33:49 IST)
+ */
+    package au.edu.unsw.sltf.services;
+    /**
+     *  CurrencyConvertMarketDataServiceSkeleton java skeleton for the axisService
+     */
+    public class CurrencyConvertMarketDataServiceSkeleton implements CurrencyConvertMarketDataServiceSkeletonInterface{
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param currencyConvertMarketData0 
+             * @return currencyConvertMarketDataResponse1 
+             * @throws CurrencyConvertMarketDataFaultException 
+         */
+        
+                 public au.edu.unsw.sltf.services.CurrencyConvertMarketDataResponseDocument currencyConvertMarketData
+                  (
+                  au.edu.unsw.sltf.services.CurrencyConvertMarketDataDocument currencyConvertMarketData0
+                  )
+            throws CurrencyConvertMarketDataFaultException{
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#currencyConvertMarketData");
+        }
+     
+    }
+    
---
+++ CurrencyConvertService/src/au/edu/unsw/sltf/services/CurrencyConvertMarketDataServiceSkeletonInterface.java
@@ -0,0 +1,30 @@
+
+/**
+ * CurrencyConvertMarketDataServiceSkeletonInterface.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.2  Built on : Apr 17, 2012 (05:33:49 IST)
+ */
+    package au.edu.unsw.sltf.services;
+    /**
+     *  CurrencyConvertMarketDataServiceSkeletonInterface java skeleton interface for the axisService
+     */
+    public interface CurrencyConvertMarketDataServiceSkeletonInterface {
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                    * @param currencyConvertMarketData
+             * @throws CurrencyConvertMarketDataFaultException : 
+         */
+
+        
+                public au.edu.unsw.sltf.services.CurrencyConvertMarketDataResponseDocument currencyConvertMarketData
+                (
+                  au.edu.unsw.sltf.services.CurrencyConvertMarketDataDocument currencyConvertMarketData
+                 )
+            throws CurrencyConvertMarketDataFaultException;
+        
+         }
+    
